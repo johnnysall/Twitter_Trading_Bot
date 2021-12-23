d17e3fe5c32029426d9d6da3b3267b6ab04d3179
JohnnySall <johnboy109@icloud.com>
2021-12-23 18:30:42 +0000

    Trying React & Working ticker buyer

--- Main/Main.py
+++ Main/Main.py
@@ -2,28 +2,15 @@ import tkinter
 from tkinter import *
 import tweepy
 import alpaca_trade_api as tradeapi
-
-Alpaca_Endpoint = "https://paper-api.alpaca.markets"
-Alpaca_API_Key = "PKPKLT6IQAA8XS3D88Q0"
-Alpaca_Secret_Key = "zRQ7pCN01WLGDXToqPlnKiveTYzRikZlvgToCgow"
+from Credentials import *
 
 Alpaca_API = tradeapi.REST(Alpaca_API_Key, Alpaca_Secret_Key, Alpaca_Endpoint)
 Alpaca_Account = Alpaca_API.get_account()
 
-API_KEY = '1ERko18WmPT8CrcvtR14WImpm'
-API_SECRET_KEY = 'aCQ8j5mc4lvOOmVZSqvHCD6X1fANQxzim23lz2lVuzFOYqIlCE'
-ACCESS_TOKEN = '1337392050803761154-ma914HbmzzukYazAD8NFqk5yICoOjM'
-ACCESS_TOKEN_SECRET = 'M7sReDo1EZMQgkDOheokAxpR9ecaoJKbVLX808mAgTnq9'
+Twitter_Auth = tweepy.OAuthHandler(Twitter_API_Key, Twitter_Secret_API_Key)
+Twitter_Auth.set_access_token(Twitter_Access_Token, Twitter_Access_Token_Secret)
 
-Twitter_API_Key = '1ERko18WmPT8CrcvtR14WImpm'
-Twitter_Secret_API_Key = 'aCQ8j5mc4lvOOmVZSqvHCD6X1fANQxzim23lz2lVuzFOYqIlCE'
-Twitter_Access_Token = '1337392050803761154-ma914HbmzzukYazAD8NFqk5yICoOjM'
-Twitter_Access_Token_Secret = 'M7sReDo1EZMQgkDOheokAxpR9ecaoJKbVLX808mAgTnq9'
-
-auth = tweepy.OAuthHandler(Twitter_API_Key, Twitter_Secret_API_Key)
-auth.set_access_token(Twitter_Access_Token, Twitter_Access_Token_Secret)
-
-api = tweepy.API(auth)
+Twitter_API = tweepy.API(Twitter_Auth)
 
 window = Tk()
 
@@ -63,41 +50,50 @@ def Buy_Stock():
     except ValueError:
         validation.config(text="Enter a number")
 
-    
-
+#Label + Entry for Ticker
 Ticker_To_Buy_Label = Label(window, text="Ticker:")
 Ticker_To_Buy_Label.pack()
 Ticker_To_Buy_Entry = Entry(window)
 Ticker_To_Buy_Entry.pack()
 
+#Label + Entry for Quantity
 Quantity_To_Buy_Label = Label(window, text="Quantity:")
 Quantity_To_Buy_Label.pack()
 Quantity_To_Buy_Entry = Entry(window, textvariable="Quanity")
 Quantity_To_Buy_Entry.pack()
 
+#Label + Entry for Side
 Side_To_Buy_Label = Label(window, text="Side:")
 Side_To_Buy_Label.pack()
 Side_To_Buy_Entry = Entry(window, textvariable="Side")
 Side_To_Buy_Entry.pack()
 
+#Buy Button
 Ticker_To_Buy_Button = Button(window, text="Buy", command=Buy_Stock)
 Ticker_To_Buy_Button.pack()
 
+#Label for the validation text e.g. Buy Complete
 validation = Label(window, text="")
 validation.pack()
 
+TweetListbox = Listbox(window, width=140)
+
+TweetListbox.pack()
+
 userID = "MrZackMorris"
+tweets = Twitter_API.user_timeline(screen_name=userID, 
+    # 200 is the maximum allowed count
+    count=200,
+    include_rts = False,
+    # Necessary to keep full_text 
+    # otherwise only the first 140 words are extracted
+    tweet_mode = 'extended'
+    )
 
-tweets = api.user_timeline(screen_name=userID, 
-                           # 200 is the maximum allowed count
-                           count=200,
-                           include_rts = False,
-                           # Necessary to keep full_text 
-                           # otherwise only the first 140 words are extracted
-                           tweet_mode = 'extended'
-                           )
-
+Tweetnum = 0
 for info in tweets[:10]:
+    TweetListbox.insert(Tweetnum, info.full_text)
     Label(window, text=info.full_text).pack()
+    Tweetnum += 1
 
 window.mainloop()
