06ac81ba1974e12153ea433c6021a47ec3c09f38
JohnnySall <johnboy109@icloud.com>
2021-12-24 13:43:21 +0000

    Added all correct folders

---
+++ TestingReactPython/Credentials.py
@@ -0,0 +1,8 @@
+Twitter_API_Key = '1ERko18WmPT8CrcvtR14WImpm'
+Twitter_Secret_API_Key = 'aCQ8j5mc4lvOOmVZSqvHCD6X1fANQxzim23lz2lVuzFOYqIlCE'
+Twitter_Access_Token = '1337392050803761154-ma914HbmzzukYazAD8NFqk5yICoOjM'
+Twitter_Access_Token_Secret = 'M7sReDo1EZMQgkDOheokAxpR9ecaoJKbVLX808mAgTnq9'
+
+Alpaca_Endpoint = 'https://paper-api.alpaca.markets'
+Alpaca_API_Key = 'PKPKLT6IQAA8XS3D88Q0'
+Alpaca_Secret_Key = 'zRQ7pCN01WLGDXToqPlnKiveTYzRikZlvgToCgow'
