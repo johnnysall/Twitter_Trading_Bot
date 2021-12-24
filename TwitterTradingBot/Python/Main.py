45dd9c7ad4bca09263535667a2f2c1c0eaea2b4b
JohnnySall <johnboy109@icloud.com>
2021-12-24 23:23:15 +0000

    Many Changes - Added all folders, and python functions

--- TwitterTradingBot/Python/Main.py
+++ TwitterTradingBot/Python/Main.py
@@ -4,24 +4,28 @@ import tweepy
 import alpaca_trade_api as tradeapi
 from Credentials import *
 import eel
+import pandas as pd
+import datetime
 
-eel.init('C:/Users/Johnny Salloway/Documents/Coding/GitHub/Twitter_Trading_Bot/TestingReactPython')
+# Configuring Eel ----------------------------------
+eel.init('C:/Users/Johnny Salloway/Documents/Coding/GitHub/Twitter_Trading_Bot/TwitterTradingBot/Web', allowed_extensions=['.js', '.html','.css'])
 
+# Configuring Alpaca API ---------------------------
 Alpaca_API = tradeapi.REST(Alpaca_API_Key, Alpaca_Secret_Key, Alpaca_Endpoint)
 Alpaca_Account = Alpaca_API.get_account()
 
+# Configuring Twitter API ---------------------------
 Twitter_Auth = tweepy.OAuthHandler(Twitter_API_Key, Twitter_Secret_API_Key)
 Twitter_Auth.set_access_token(Twitter_Access_Token, Twitter_Access_Token_Secret)
-
 Twitter_API = tweepy.API(Twitter_Auth)
 
 # Get a list of all of our positions.
 portfolio = Alpaca_API.list_positions()
 
-# Print the quantity of shares for each position.
-for position in portfolio:
-    print("{} shares of {}".format(position.qty, position.symbol))
+#for position in portfolio:
+#    print("{} shares of {}".format(position.qty, position.symbol))
 
+# Find Tweets by "userID" ----------------------------
 userID = "MrZackMorris"
 tweets = Twitter_API.user_timeline(screen_name=userID, 
     # 200 is the maximum allowed count
@@ -29,9 +33,9 @@ tweets = Twitter_API.user_timeline(screen_name=userID,
     include_rts = False,
     # Necessary to keep full_text 
     # otherwise only the first 140 words are extracted
-    tweet_mode = 'extended'
-    )
+    tweet_mode = 'extended')
 
+# Function to add Tweets to list ------------------
 @eel.expose
 def FindTweetsPY():
     TweetList = []
@@ -39,9 +43,54 @@ def FindTweetsPY():
         TweetList.append(info.full_text)
     return TweetList
 
+# Find all completed orders through Alpaca API ----------
+ClosedOrders = Alpaca_API.list_orders(
+        status="closed",
+        limit=100)
+
+# Function to Add all the Orders to the list -------------
 @eel.expose
-def testing(text):
-    print(text)
+def FindOrdersPY():
+    OrderList = []
+
+    ClosedOrders = Alpaca_API.list_orders(
+        status="closed",
+        limit=100)
+
+    for info in ClosedOrders:
+        SubList = []
+        SubList.append(info.symbol)
+        SubList.append(info.qty)
+        SubList.append(info.side)
+        SubList.append(info.status)
+        OrderList.append(SubList)
+    return OrderList
+
+# Market Order sent to Alpaca API (Side = Buy/ Short/ Sell)
+@eel.expose
+def Alpaca_Order(symbol, qty, side):
+    Alpaca_API.submit_order(
+        symbol=symbol.upper(),
+        qty=qty,
+        side=side.lower(),
+        type='market',
+        time_in_force='gtc')
+
+# Pull Portfolio from Alpaca API -----------------------
+Portfolio = Alpaca_API.list_positions()
+@eel.expose
+def GetPortfolioPY():
+    PortfolioList = []
+
+    for info in Portfolio:
+        SubList = []
+        SubList.append(info.symbol)
+        SubList.append(info.qty)
+        SubList.append(float(info.market_value)/float(info.qty))
+        PortfolioList.append(SubList)
+    return PortfolioList
 
 
-eel.start('Main.html')
+# Start eel - establish connection from Python to Javascript
+eel.start('Index.html')
+#eel.start('Index.html', mode='chrome', cmdline_args=['--kiosk'])
--- TwitterTradingBot/Web/Javascript/Main.js
+++ TwitterTradingBot/Web/Javascript/Main.js
@@ -9,16 +9,54 @@ async function getTweets() {
     }
 }
 
+async function getOrders() {
+  let OrderArray = await eel.FindOrdersPY()()
+
+  var table = document.getElementById("OrdersTBL").getElementsByTagName('tbody')[0];;
+  while (table.rows.length > 1) {
+    table.deleteRow(0);
+  }
+  for (var i=0; i<OrderArray.length; i++) {
+    var row = table.insertRow();
+    for (var x=0; x<4; x++){
+      var cell = row.insertCell(x);
+      cell.innerHTML = OrderArray[i][x];
+    }
+  }
+}
+
+async function getPortfolio() {
+  let PortfolioArray = await eel.GetPortfolioPY()()
+
+  var table = document.getElementById("PortfolioTBL").getElementsByTagName('tbody')[0];
+  while (table.rows.length > 1) {
+    table.deleteRow(1);
+  }
+  for (var i=0; i<PortfolioArray.length; i++) {
+    var row = table.insertRow();
+    for (var x=0; x<4; x++){
+      if (x<3) {
+        var cell = row.insertCell(x);
+        cell.innerHTML = PortfolioArray[i][x];
+      } 
+      else {
+        var cell = row.insertCell(x);
+        cell.innerHTML = "<button id='Sellbtn';>Sell</button>";
+        var cell = row.insertCell(x);
+        cell.innerHTML = "<button id='Buybtn';>Buy</button>";
+      }
+    }
+  }
+}
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("SideNav").style.width = "250px";
-    document.getElementById("MainPage").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
   
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("SideNav").style.width = "0";
-    document.getElementById("MainPage").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-  }
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
