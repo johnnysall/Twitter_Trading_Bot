3928b04cc7c923d5a40c706e0f69cce1ebd6ea45
johnnysall <54597196+johnnysall@users.noreply.github.com>
2022-08-30 14:17:13 +0100

    Remove Secrets

--- TwitterTradingBot/Python/Credentials.py
+++ TwitterTradingBot/Python/Credentials.py
@@ -1,8 +1,9 @@
-Twitter_API_Key = '1ERko18WmPT8CrcvtR14WImpm'
-Twitter_Secret_API_Key = 'aCQ8j5mc4lvOOmVZSqvHCD6X1fANQxzim23lz2lVuzFOYqIlCE'
-Twitter_Access_Token = '1337392050803761154-ma914HbmzzukYazAD8NFqk5yICoOjM'
-Twitter_Access_Token_Secret = 'M7sReDo1EZMQgkDOheokAxpR9ecaoJKbVLX808mAgTnq9'
+# Add These for it to work
+Twitter_API_Key = ''
+Twitter_Secret_API_Key = ''
+Twitter_Access_Token = ''
+Twitter_Access_Token_Secret = ''
 
-Alpaca_Endpoint = 'https://paper-api.alpaca.markets'
-Alpaca_API_Key = 'PKPKLT6IQAA8XS3D88Q0'
-Alpaca_Secret_Key = 'zRQ7pCN01WLGDXToqPlnKiveTYzRikZlvgToCgow'
+Alpaca_Endpoint = ''
+Alpaca_API_Key = ''
+Alpaca_Secret_Key = ''
